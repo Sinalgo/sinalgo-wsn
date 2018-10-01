5795ca9c53c020b5c63149945c5bfff4c4e508f3
Andre Brait <andrebrait@gmail.com>
2018-10-01 13:07:49 -0300

    Enhancements to the automated testing

--- automatedTests.pl
+++ automatedTests.pl
@@ -12,23 +12,25 @@
 
 $numRepetitions = 20;  # Number of rounds to perform for each simulation
 $refreshRate = 10; # Refresh rate
-$numNodes = 36;
 $failureDetectionModelSuccessRate = 1.0;
+@numNodesList = (36, 49, 64, 81, 100);
 
-# run the simulation for different node speeds (2, 5, 8)
-for($i=0; $i<$numRepetitions; $i+=1) {
+for $numNodes (@numNodesList) {
+  # run the simulation for different node speeds (2, 5, 8)
+  for($i=0; $i<$numRepetitions; $i+=1) {
 
-  # and run with some different node densities (200, 300, 400, 500 nodes)
-  die "Terminated prematurely" unless
-    system("./gradlew run -PappArgs=\"[" .
-  		 "'-project', 'tcc', " .             # choose the project
-  		 "'-gen', '$numNodes', 'tcc:SensorNode', 'tcc:ListBasedPositionModel', 'NoMobility', " . # generate nodes
-  		 "'-gen', '1', 'tcc:SinkNode', 'tcc:ListBasedPositionModel', 'NoMobility', " . # generate nodes
-  		 "'-overwrite', 'failureDetectionModelSuccessRate=$failureDetectionModelSuccessRate', " .  # Detection rate
-  		 "'exitOnTerminationInGUI=true', " .  # Close GUI when hasTerminated() returns true
-  		 "'outputToConsole=false', " .        # Create a framework log-file for each run
-  		 "'-refreshRate', '$refreshRate', " . # Don't draw GUI often
-  		 "'-batch']\" > ~/saida-rssf/sim\\ $numNodes\\ $i.txt"                        # Use batch
-  		) == 0;
+    # and run with some different node densities (200, 300, 400, 500 nodes)
+    die "Terminated prematurely" unless
+      system("./gradlew run -PappArgs=\"[" .
+    		 "'-project', 'tcc', " .             # choose the project
+    		 "'-gen', '$numNodes', 'tcc:SensorNode', 'tcc:ListBasedPositionModel', 'NoMobility', " . # generate nodes
+    		 "'-gen', '1', 'tcc:SinkNode', 'tcc:ListBasedPositionModel', 'NoMobility', " . # generate nodes
+    		 "'-overwrite', 'failureDetectionModelSuccessRate=$failureDetectionModelSuccessRate', " .  # Detection rate
+    		 "'exitOnTerminationInGUI=true', " .  # Close GUI when hasTerminated() returns true
+    		 "'outputToConsole=false', " .        # Create a framework log-file for each run
+    		 "'-refreshRate', '$refreshRate', " . # Don't draw GUI often
+    		 "'-batch']\" > ~/saida-rssf/sim\\ $numNodes\\ $i.txt"                        # Use batch
+    		) == 0;
 
+  }
 }
